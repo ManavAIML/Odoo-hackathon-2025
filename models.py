d3a120219af8ba835f7ac048a42219227bcec5d6
kushal patel <kushalpatel1596@gmail.com>
2025-07-12 17:37:19 +0530

    Update modelsf.py

--- models.py
+++ models.py
@@ -5,6 +5,23 @@ class User(db.Model):
     username = db.Column(db.String(150), unique=True, nullable=False)
     email = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
+    items = db.relationship('Item', backref='owner', lazy=True)
 
     def __repr__(self):
         return f"<User {self.username}>"
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    gender = db.Column(db.String(10), nullable=False)
+    category = db.Column(db.String(100), nullable=False)
+    type = db.Column(db.String(100))
+    size = db.Column(db.String(10), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    tags = db.Column(db.String(200))
+    image_filename = db.Column(db.String(200))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"<Item {self.title}>"
